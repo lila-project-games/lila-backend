256d5973cc70b03a37f34da42c540b69c3c3809d
Andrea103 <andreasierra103@gmail.com>
2023-09-19 10:14:23 +0200

    first commit

---
+++ src/main/java/backend/lilaproject/LilaProjectApplication.java
@@ -0,0 +1,13 @@
+package backend.lilaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/lilaproject/LilaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.lilaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
