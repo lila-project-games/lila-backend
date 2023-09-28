8286a549663490c9cf220f1075610ece8d52920e
dosdedoce <dosdedoce@gmail.com>
2023-09-28 11:27:38 +0200

    models

--- src/main/java/backend/lilaproject/models/Course.java
+++ src/main/java/backend/lilaproject/models/Course.java
@@ -1,4 +1,28 @@
 package backend.lilaproject.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCourse;
+
+    @Column(nullable = false)
+    private int level;
+    @Column(nullable = false)
+    private char courseClass;
+
+    @ManyToOne
+    @JoinColumn(name = "idSchool")
+    School school;
+
 }
--- src/main/java/backend/lilaproject/models/Student.java
+++ src/main/java/backend/lilaproject/models/Student.java
@@ -1,4 +1,36 @@
 package backend.lilaproject.models;
 
+import backend.lilaproject.models.Course;
+import backend.lilaproject.models.School;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idStudent;
+
+    @Column(nullable = false)
+    private String studentName;
+
+    @Column(nullable = false)
+    private String firstSurname;
+
+    @Column(nullable = false)
+    private String secondSurname;
+
+    @ManyToOne
+    @JoinColumn(name = "idCourse")
+    Course course;
+
 }
+
